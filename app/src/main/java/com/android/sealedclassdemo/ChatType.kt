208f95af44e791712c19541daaaa5f1e84f74499
Frederico Gassen <fredericogassen@gmail.com>
2021-06-17 23:49:16 -0300

    Update ChatType.kt

--- app/src/main/java/com/android/sealedclassdemo/ChatType.kt
+++ app/src/main/java/com/android/sealedclassdemo/ChatType.kt
@@ -4,6 +4,6 @@ import java.io.Serializable
 import java.util.Date
 
 sealed class ChatType : Serializable {
-    data class Personal(val personName: String,val status: String) : ChatType()
+    data class Personal(val personName: String, val status: String) : ChatType()
     data class Group(val groupName: String, val description: String, val creationDate: Date) : ChatType()
 }
